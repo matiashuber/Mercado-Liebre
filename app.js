d6d4f57e21922ae6ede53408c7d12206cee37597
matiashuber <matiihy300@gmail.com>
2023-02-15 00:36:03 -0300

    fix scripts package.json

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, "./public")));
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended:false}));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
